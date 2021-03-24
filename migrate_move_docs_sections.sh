c1da650e35837c57c1308dfc3914c80fa4c0983e
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2021-03-24 11:30:46 +0100

    Work

---
+++ migrate_move_docs_sections.sh
@@ -0,0 +1,4 @@
+docs="content/en/documentation"
+dirs=("functions", "maintenance", "hosting-and-deployment", "hugo-modules", "myshowcase", "readfiles", "tools", "troubleshooting", "variables", "commands", "content-management","getting-started", "hugo-pipes", "templates")
+mkdir -p $docs
+
